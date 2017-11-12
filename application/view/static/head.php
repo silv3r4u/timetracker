2af5509d277e644c493b59dcafeb3987a58b6033
silv3r4u <nnsilviu@gmail.com>
2017-11-12 21:55:40 +0200

    Create head.php

---
+++ application/view/static/head.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from demo.naksoid.com/elephant/flaming-red/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 03 Nov 2016 14:01:15 GMT -->
+<head>
+    <meta charset="UTF-8">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php echo $title;?></title>    
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url();?>apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="<?php echo base_url();?>favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="<?php echo base_url();?>favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="manifest.json">
+    <link rel="mask-icon" href="<?php echo base_url();?>safari-pinned-tab.svg" color="#0288d1">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,400italic,500,700">
+    <link rel="stylesheet" href="<?php echo base_url();?>css/vendor.minf9e3.css?v=1.1">
+    <link rel="stylesheet" href="<?php echo base_url();?>css/elephant.minf9e3.css?v=1.1">
+    <link rel="stylesheet" href="<?php echo base_url();?>css/application.minf9e3.css?v=1.1">
+    <link rel="stylesheet" href="<?php echo base_url();?>css/sweetalert.css">
+</head>
